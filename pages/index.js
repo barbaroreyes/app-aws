bbfabea0f2d10f72b7a006bd046d86bc748f0c7c
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-01-25 23:38:31 -0600

    update

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,31 @@
-
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {useState , useEffect} from 'react'
-import Link from  'next/link';
-import { DataStore } from '@aws-amplify/datastore';
-import { Post } from '../src/models';
+import { DataStore } from 'aws-amplify'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { Post } from '../src/models'
 
 export default function Home() {
-  const [posts, setPost] = useState([])
-  useEffect(()=>{
-    fetchPost()
-    async function fetchPost( ){
-      const postData = await DataStore.query(Post);
-      setPost(postData)
-  
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetchPosts()
+    async function fetchPosts() {
+      const postData = await DataStore.query(Post)
+      setPosts(postData)
     }
-  },[])
-  console.log(posts , 'posts');
+    DataStore.observe(Post).subscribe(() => fetchPosts())
+  }, [])
   return (
     <div className={styles.container}>
-      <h2>Posts</h2>
-     {
-       posts.map( post =>(
-         <Link href = {`/posts/${post.id}`}>
-         <a>
-           <h1>{post.content}</h1>
-        </a></Link>
-        
-       ))
-     }
+      <h1>Posts</h1>
+      {
+        posts.map(post => (
+          <Link href={`/posts/${post.id}`}>
+            <a>
+              <h2>{post.title}</h2>
+            </a>
+          </Link>
+        ))
+      }
     </div>
   )
 }
